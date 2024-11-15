88859455c167de427f59c40f0842a353d3e94cf4
Raul Martinez <rauldeoran17@gmail.com>
2024-11-15 19:52:42 +0000

    add projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,12 @@ export default {
         'dot-light': '#e2e8f0',
         'dot-dark': '#334155',
       },
+      screens: {
+        lg835: '835px',
+      },
+      fontSize: {
+        xxs: '0.625rem',
+      },
     },
   },
 	plugins: [],
